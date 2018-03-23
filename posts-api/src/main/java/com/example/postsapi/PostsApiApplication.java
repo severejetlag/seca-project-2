d5d5c96dee56b0802176fa09599fc6d251c59bbf
severejetlag <nhl1013@gmail.com>
2018-03-23 14:52:40 -0700

    Added dependecies for posts-api along with application-dev.properties for Eureka and posstgres config

---
+++ posts-api/src/main/java/com/example/postsapi/PostsApiApplication.java
@@ -0,0 +1,21 @@
+package com.example.postsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class PostsApiApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "some users";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsApiApplication.class, args);
+	}
+}
